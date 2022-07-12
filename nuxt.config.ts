875f554db38450b954c41088044233436b66264c
Paul Soulhiard <contact@p-soulhiard.fr>
2022-07-12 21:02:41 +0200

    v1 shopify+nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,29 @@
-import { defineNuxtConfig } from 'nuxt3'
+import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  ssr: false,
   css: [
     '@/assets/fonts/fonts.scss',
     '@/assets/styles/main.scss',
     '@/assets/styles/reset.scss'
-  ]
+  ],
+  modules: ['nuxt-graphql-client'],
+  runtimeConfig: {
+    public: {
+      "graphql-client": {
+        clients: {
+          default: {
+            host: process.env.SHOPIFY_DOMAIN,
+            token: {
+              name: "X-Shopify-Storefront-Access-Token",
+              value: process.env.SHOPIFY_STOREFRONT_TOKEN,
+              type: null,
+            },
+            retainToken: true,
+          },
+        },
+      },
+    },
+  },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,6 @@
 </template>
 
 <script setup>
+  const { data } = await useAsyncData('products', () => GqlProducts({ first: 1 }))
   console.log('Hello !')
 </script>
