eeb0f8f984abaa6c690b4e05d3c2c392b1401af9
Paul Soulhiard <contact@p-soulhiard.fr>
2022-07-17 17:57:41 +0200

    head + meta

--- app.vue
+++ app.vue
@@ -1,3 +1,25 @@
 <template>
+  <Head>
+    <Title> Nuxt Boilerplate </Title>
+    <Meta name="description" content=" A Basic Nuxt Boilerplate " />
+    <Meta name="theme-color" content="#4285f4" />
+
+    <Meta property="og:url" content="https://nuxt" />
+    <Meta property="og:type" content="website" />
+    <Meta property="og:title" content="Content Title" />
+    <Meta property="og:site_name" content="Site Name" />
+    <Meta property="og:description" content="Description Here" />
+    <Meta property="og:image" content="https://example.com/share.png" />
+
+    <Meta name="twitter:card" content="summary" />
+    <Meta name="twitter:site" content="@site_account" />
+    <Meta name="twitter:url" content="https://example.com/page.html" />
+    <Meta name="twitter:title" content="Content Title" />
+    <Meta name="twitter:description" content="Content description less than 200 characters" />
+    <Meta name="twitter:image" content="https://example.com/share.jpg" />
+
+    <Link rel="icon" sizes="192x192" href="/icon_192.png" />
+    <Link rel="apple-touch-icon" href="/icon_192.png" />
+  </Head>
   <NuxtPage />
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,30 @@
 import { defineNuxtConfig } from 'nuxt'
+import glsl from 'vite-plugin-glsl'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+
   ssr: false,
+
   css: [
     '@/assets/fonts/fonts.scss',
     '@/assets/styles/main.scss',
     '@/assets/styles/reset.scss'
   ],
+
   modules: ['nuxt-graphql-client'],
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/styles/_variables.scss";'
+        },
+      },
+    },
+    plugins: [glsl()],
+  },
+
   runtimeConfig: {
     public: {
       "graphql-client": {
